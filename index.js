71332b15c58afa6a313d704aadc1e051f339ee0e
HentaiLiu <hentailiu@kugou.net>
2017-07-13 16:30:20 +0800

    update

--- index.js
+++ index.js
@@ -3,34 +3,30 @@ const parser = require('./src/parser-babylon');
 const fs = require("fs");
 const path = require("path");
 const astUtil = require("./src/astUtil");
-function parseComponent(file){
+function parseComponent(file) {
     const ast = parser(file);
     let model;
     // fs.writeFileSync(path.resolve(__dirname,"test","test.json"),JSON.stringify(ast,null,4));
-    try{
+    try {
         model = astUtil.walkAst(ast);
         return model;
-    }catch(e){
+    } catch (e) {
         console.error(e);
         process.exit(1);
     }
 }
 
 
-function parseFunction(file){
-    const text = fs.readFileSync(path.resolve(__dirname,"test","jumpToFxHome.js"), "utf8");
+function parseFunction(file) {
+    // const text = fs.readFileSync(path.resolve(__dirname,"test","jumpToFxHome.js"), "utf8");
     const ast = parser(text);
     let functionModel;
-    fs.writeFileSync(path.resolve(__dirname,"test","function-test.json"),JSON.stringify(ast,null,4));
-    try{
-        model = astUtil.walkAstForFunction(ast);
-        return model;
-    }catch(e){
-        console.error(e);
-    }
+    // fs.writeFileSync(path.resolve(__dirname,"test","function-test.json"),JSON.stringify(ast,null,4));
+    model = astUtil.walkAstForFunction(ast);
+    return model;
 }
 module.exports = {
-    version:version,
-    parseComponent:parseComponent,
-    parseFunction:parseFunction
+    version: version,
+    parseComponent: parseComponent,
+    parseFunction: parseFunction
 };
